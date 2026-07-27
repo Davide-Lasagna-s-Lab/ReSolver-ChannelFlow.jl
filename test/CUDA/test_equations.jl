e024f39b18a384717366e2da0e5f775c17fffa9c
Thomas Burton <tburton5572@gmail.com>
2026-07-27 17:15:29 +0100

    reworked operator test

--- test/CUDA/test_equations.jl
+++ test/CUDA/test_equations.jl
@@ -1,6 +1,7 @@
 @testset "GPU equations                         " begin
     # construct grid
     Ny = 32; Nx = 15; Nz = 33; Nt = 51
+    M = 2
     y, ws = FDGrids.grid(Ny, -1, 1, MappedGrid(1))
     D₁ = DiffMatrix(y, 3, 1)
     D₂ = DiffMatrix(y, 3, 2)
@@ -15,23 +16,32 @@
     # construct Couette and Poiseuille equations
     Re = 10
     Ro = Float32(0.5)
-    op_nl = @test_nowarn CartesianPrimitive3DNSE( g, Re, force=CoriolisForce(Ro))
-    op_ln = @test_nowarn CartesianPrimitive3DLNSE(g, Re, force=CoriolisForce(Ro), mode=AdjointDiscrete())
+    op = @test_nowarn NSEBase.construct_equations(g,
+                                                  Re,
+                                                 (parent(g).y, nothing, nothing),
+                                                  NSEBase.CartesianPrimitive3D();
+                                                  force=CoriolisForce(Ro),
+                                                   mode=AdjointDiscrete(),
+                                                dealias=true)
 
     # check if types are correct and memory is correct place
-    @test op_nl isa CartesianPrimitive3DNSE{Float32, <:FFTPlans, <:FTField{<:CUDAExt.GPUGrid}, <:Field{<:CUDAExt.GPUGrid}}
-    @test op_ln isa CartesianPrimitive3DLNSE{AdjointDiscrete, Float32, <:FFTPlans, <:FTField{<:CUDAExt.GPUGrid}, <:Field{<:CUDAExt.GPUGrid}}
-    @test op_nl.plans.backend == CUDA.cuFFT
-    @test op_ln.plans.backend == CUDA.cuFFT
-    @test eltype(op_nl.scache) <: VectorField{3, <:FTField{<:CUDAExt.GPUGrid}}
-    @test eltype(op_nl.pcache) <: VectorField{3, <:Field{<:CUDAExt.GPUGrid}}
-    @test eltype(op_ln.scache) <: VectorField{3, <:FTField{<:CUDAExt.GPUGrid}}
-    @test eltype(op_ln.pcache) <: VectorField{3, <:Field{<:CUDAExt.GPUGrid}}
+    @test op.nl isa CartesianPrimitive3DNSE{Float32, <:FFTPlans, <:FTField{<:CUDAExt.GPUGrid}, <:Field{<:CUDAExt.GPUGrid}}
+    @test op.ln isa CartesianPrimitive3DLNSE{AdjointDiscrete, Float32, <:FFTPlans, <:FTField{<:CUDAExt.GPUGrid}, <:Field{<:CUDAExt.GPUGrid}}
+    @test op.base isa Tuple{<:CuArray{Float32, 1}, Nothing, Nothing}
+    @test op.nl.plans.backend == CUDA.cuFFT
+    @test op.ln.plans.backend == CUDA.cuFFT
+    @test eltype(op.nl.scache) <: VectorField{3, <:FTField{<:CUDAExt.GPUGrid}}
+    @test eltype(op.nl.pcache) <: VectorField{3, <:Field{<:CUDAExt.GPUGrid}}
+    @test eltype(op.ln.scache) <: VectorField{3, <:FTField{<:CUDAExt.GPUGrid}}
+    @test eltype(op.ln.pcache) <: VectorField{3, <:Field{<:CUDAExt.GPUGrid}}
+    @test eltype(op.cache1)    <: FTField{<:CUDAExt.GPUGrid}
+    @test eltype(op.cache2)    <: FTField{<:CUDAExt.GPUGrid}
 
     # check if computation completes
-    u = VectorField(g)
-    v = VectorField(g)
-    out = VectorField(g)
-    @test_nowarn op_nl(0.0, u, out)
-    @test_nowarn op_ln(0.0, u, v, out)
+    Ψ   = ntuple(_ -> CUDA.zeros(ComplexF32, M, Ny, (Nx >> 1) + 1, Nz, Nt), 3)
+    a   = ProjectedField(g, Ψ)
+    b   = ProjectedField(g, Ψ)
+    out = ProjectedField(g, Ψ)
+    @test_nowarn op(out, a)
+    @test_nowarn op(out, a, b)
 end
